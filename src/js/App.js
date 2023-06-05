b042b56dd0403a7f766b4dfdfe84d5cfd7dfdbb8
NM4TT <manuelnmatute@gmail.com>
2023-06-05 12:49:38 -0500

    reestructuracion parte 2

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,8 @@
-import logo from './platzi.webp';
+import logo from '../platzi.webp';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
-import './App.css';
+import '../css/App.css';
 
 function App() {
   return (
